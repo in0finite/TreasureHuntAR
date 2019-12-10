a308a833a4e6a0d07e227ac572ace23a02962b97
in0finite <leshow816@gmail.com>
2019-12-10 14:02:12 +0100

    ...

---
+++ Assets/Scripts/TreasureHuntManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TreasureHunt
+{
+
+    public class TreasureHuntManager : MonoBehaviour
+    {
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+    }
+
+}
