a94e7391a7726a6db3c7aeda7d40e5f779e1de1e
Darko Djordjevic <darko.djordjevic@zuehlke.com>
2021-05-28 12:13:38 +0100

    Adjust maze position

--- Assets/Scripts/Maze/PlacementIndicator.cs
+++ Assets/Scripts/Maze/PlacementIndicator.cs
@@ -6,7 +6,9 @@ using UnityEngine.XR.ARFoundation;
 public class PlacementIndicator : MonoBehaviour
 {
     private ARRaycastManager _raycastManager;
-    private GameObject _visual;
+    [SerializeField] private GameObject _visual;
+
+    private bool _instantiated = false;
 
     void Start()
     {
@@ -16,18 +18,23 @@ public class PlacementIndicator : MonoBehaviour
 
     void Update()
     {
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        _raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
-
-        if (hits.Count > 0)
+        if (_instantiated == false)
         {
-            transform.position = hits[0].pose.position;
-            transform.rotation = hits[0].pose.rotation;
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            _raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
 
-
-            if (!_visual.gameObject.activeInHierarchy)
+            if (hits.Count > 0)
             {
-                _visual.gameObject.SetActive(true);
+                _visual.transform.position = hits[0].pose.position;
+                //transform.rotation = hits[0].pose.rotation;
+
+                if (!_visual.gameObject.activeInHierarchy)
+                {
+                    _visual.gameObject.SetActive(true);
+
+                    _instantiated = true;
+
+                }
             }
         }
     }
