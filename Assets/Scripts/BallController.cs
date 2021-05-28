56c04214652d8f0230ca4146b8d488774d0bd349
Darko Djordjevic <darko.djordjevic@zuehlke.com>
2021-05-28 14:18:35 +0100

    Ball controller now uses input from joystick

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,33 +6,60 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
     private Rigidbody _rigidbody;
+    private Joystick _joystick;
+
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _joystick = FindObjectOfType<Joystick>();
     }
 
     private void Update()
     {
-        Vector3 direction = Vector3.zero;
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(KeyCode.W))
+        if (_joystick != null)
         {
-            direction = Camera.main.transform.up;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            direction = Camera.main.transform.up * -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            direction = Vector3.Cross(Camera.main.transform.up, Vector3.up);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            direction = Vector3.Cross(Camera.main.transform.up, Vector3.up) * -1;
+            Vector2 vector = _joystick.Direction;
+            x += vector.x;
+            z += vector.y;
         }
 
-        _rigidbody.velocity = Vector3.Lerp(_rigidbody.velocity, direction, _speed * Time.deltaTime);
+        var camera = Application.isEditor ? Camera.main : Camera.current;
+
+        var transformVector = camera.transform.TransformVector(new Vector3(x, 0f, z));
+
+        float length = transformVector.magnitude;
+
+        transformVector.y = 0;
+
+        transformVector = transformVector.normalized * length;
+
+        _rigidbody.velocity = Vector3.Lerp(_rigidbody.velocity, transformVector, _speed * Time.deltaTime);
+
+        #region legacy
+        //Vector3 direction = Vector3.zero;
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    direction = Camera.main.transform.up;
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    direction = Camera.main.transform.up * -1;
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    direction = Vector3.Cross(Camera.main.transform.up, Vector3.up);
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    direction = Vector3.Cross(Camera.main.transform.up, Vector3.up) * -1;
+        //}
+
+        //_rigidbody.velocity = Vector3.Lerp(_rigidbody.velocity, direction, _speed * Time.deltaTime);
+        #endregion
     }
 }
